9b5a842b5be7552ecc90e2b8ff43a564dede8b15
Nobie12 <kenaule1@gmail.com>
2025-07-15 12:58:12 +0300

    logging decorator

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,9 +1,11 @@
-def log_queries():
-    def decorator(func):
-        def wrapper(*args, **kwargs):
-            query = args[0] if args else kwargs.get('query', '')
-            print(f"Executing SQL Query: {query}")
-            return func(*args, **kwargs)
-        return wrapper
-    return decorator
+from datetime import datetime
+import functools
+
+def log_queries(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        query = kwargs.get("query") or (args[0] if args else "UNKNOWN QUERY")
+        print(f"[{datetime.now()}] Executing SQL Query: {query}")
+        return func(*args, **kwargs)
+    return wrapper
 
