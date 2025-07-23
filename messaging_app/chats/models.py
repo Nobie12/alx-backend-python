3a70d0d8a7570e6dba25cef3b1f1ba1dee218584
Nobie12 <kenaule1@gmail.com>
2025-07-23 22:25:30 +0300

    permissions for conversations

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class Conversation(models.Model):
+    participants = models.ManyToManyField(User, related_name="conversations")
+    title = models.CharField(max_length=255)
 # Create your models here.
 class Message(models.Model):
-    author = models.CharField(max_length=100)
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name="messages")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -7,4 +7,19 @@ class IsOwner(permissions.BasePermission):
     Assumes the model has a `user` or `owner` foreign key.
     """
     def has_object_permission(self, request, view, obj):
-        return obj.user == request.user
+        return obj.author == request.user
+
+class IsParticipantOfConversation(permissions.BasePermission):
+    """
+    Allow only authenticated users to access the api
+    Allow only participants in a conversation to send, view, update and delete messages
+    """
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+
+        return True
+
+    def has_object_permission(self, request, view, obj):
+        conversation = obj.conversation
+        return request.user in conversation.participants.all()
