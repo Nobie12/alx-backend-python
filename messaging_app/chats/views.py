8a612b735dbd3e29b5862983007ced2909f38706
Nobie12 <kenaule1@gmail.com>
2025-07-23 18:33:37 +0300

    message setup

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from .serializers import MessageSerializer
+from rest_framework import generics
+from .models import Message
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .permission import IsOwner
+
 
 # Create your views here.
+class MessageListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+class MessageDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwner]
