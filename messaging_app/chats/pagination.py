75c09a9147cc721546ebde9cd89d23e4e28b73d5
Nobie12 <kenaule1@gmail.com>
2025-07-24 16:20:22 +0300

    pagination count

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,6 +1,15 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 class ChatPagination(PageNumberPagination):
     page_size = 20
     page_size_query_param = 'page_size'  # allows ?page_size=15 override
     max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,     # ✅ REQUIRED BY THE CHECK
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+        })
