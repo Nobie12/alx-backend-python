24407059321745ed676ba190dd01cb3711682ea8
Nobie12 <kenaule1@gmail.com>
2025-07-26 14:55:01 +0300

    use serializer validation

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -3,10 +3,28 @@ from .models import Message, Conversation, CustomUser
 
 
 class RegisterSerializer(serializers.ModelSerializer):
+    # Explicitly define fields to use CharField
+    username = serializers.CharField()
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+    phone_number = serializers.CharField(required=False)
+    first_name = serializers.CharField(required=False)
+    last_name = serializers.CharField(required=False)
+    role = serializers.CharField()
+
+    full_name = serializers.SerializerMethodField()  # 👈 for SerializerMethodField
+
     class Meta:
         model = CustomUser
-        fields = ['user_id', 'username', 'password', 'email', 'first_name', 'last_name', 'phone_number', 'role']
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ['user_id', 'username', 'password', 'email', 'first_name', 'last_name', 'phone_number', 'role', 'full_name']
+
+    def get_full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
+
+    def validate_email(self, value):
+        if CustomUser.objects.filter(email=value).exists():
+            raise serializers.ValidationError("This email is already registered.")  # 👈 ValidationError
+        return value
 
     def create(self, validated_data):
         return CustomUser.objects.create_user(**validated_data)
