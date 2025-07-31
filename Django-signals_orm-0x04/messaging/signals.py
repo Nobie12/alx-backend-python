0780a87f09d0e56d95135b0f6eb7ec42c63d4013
Nobie12 <kenaule1@gmail.com>
2025-07-31 16:39:17 +0300

    minor tweak

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -4,4 +4,4 @@ from django.dispatch import receiver
 
 @receiver(post_save, sender=Message)
 def create_notification(sender, instance, **kwargs):
-    Notification.objects.create(User=instance.receiver, Message=instance);
+    Notification.objects.create(User=instance.receiver, message=instance);
