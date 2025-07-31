bdcc4d00e0538cdcc97aec7f712a1029e9713573
Nobie12 <kenaule1@gmail.com>
2025-07-31 16:57:04 +0300

    logging message edited

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,7 +1,31 @@
-from .models import Message, Notification
-from django.db.models.signals import post_save
+from .models import Message, Notification, MessageHistory
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
+from django.utils import timezone
 
 @receiver(post_save, sender=Message)
 def create_notification(sender, instance, **kwargs):
     Notification.objects.create(User=instance.receiver, message=instance);
+
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    if instance.edited:
+        Message.object.update(edited=True)
+        MessageHistory.object.update(message=instance)
+
+    try:
+        old_instance = Message.objects.get(pk=instance.pk)
+    except Message.DoesNotExist:
+        # It's a new message, not an edit
+        return
+
+    if old_instance.content != instance.content:
+        instance.edited = True
+        instance.edited_at = timezone.now()
+
+        MessageHistory.objects.create(
+            message=instance,
+            old_content=old_instance.content,
+            edited_at=timezone.now(),
+            edited_by=instance.sender,
+        )
