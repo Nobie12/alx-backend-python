5f9144866e78ccd75934e27f703474a58f99b83c
Nobie12 <kenaule1@gmail.com>
2025-07-31 20:13:51 +0300

    minor tweaks

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -8,8 +8,45 @@ from django.views.decorators import cache
 
 User = get_user_model()
 
+def build_thread(message):
+    return {
+        "id": str(message.id),
+        "sender": message.sender.username,
+        "receiver": message.receiver.username,
+        "content": message.content,
+        "created_at": message.created_at,
+        "replies": [build_thread(reply) for reply in message.replies.all()]
+    }
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    # GET /messages/threads/
+    @action(detail=False, methods=["get"], url_path="threads")
+    def get_threaded_messages(self, request):
+
+        top_level_messages = (
+            Message.objects.filter(
+                parent_message__isnull=True,
+                sender=request.user 
+            )
+            .select_related("sender", "receiver")
+            .prefetch_related(
+                "replies",                # Prefetch direct replies
+                "replies__sender",        # Prefetch sender of replies
+                "replies__receiver",      # Prefetch receiver of replies
+                "replies__replies",       # Prefetch second-level replies
+            )
+        )
+
+        threads = [build_thread(msg) for msg in top_level_messages]
+        return Response(threads, status=status.HTTP_200_OK)
+
+
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objectsall()
+    queryset = User.objects.all()
     permission_classes = [IsAuthenticated]
 
     @action(detail=False, methods=["delete"], url_path="delete-account")
